f72a33e9c60205e0aa806bfef7c63df7070829d5
alaikal02 <alaikal.fajri@gmail.com>
2024-08-19 20:58:59 +0700

    feat(pageReport): menambahkan page untuk melihat seluruh progress

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:khataman/features/khataman/presentation/pages/page_anggota.dart';
 import 'features/khataman/presentation/pages/splash_screen.dart';
 import 'features/khataman/presentation/pages/page_task.dart';
+import 'features/khataman/presentation/pages/page_report.dart';
 
 void main() {
   runApp(KhatamanApp());
@@ -22,6 +23,12 @@ class KhatamanApp extends StatelessWidget {
         '/task': (context) => PageTask(
             members:
                 ModalRoute.of(context)!.settings.arguments as List<String>),
+        '/report': (context) {
+          final Map<String, List<bool>> tasks = ModalRoute.of(context)!
+              .settings
+              .arguments as Map<String, List<bool>>;
+          return PageReport(tasks: tasks);
+        },
       },
     );
   }
